befa1b838fef63670ee371f5670e57c5019b52de
Petrus Briland <petrus.jonsson@gmail.com>
2021-05-26 22:29:39 +0200

    Include user who wanted to play ranked

--- commands/rankat.js
+++ commands/rankat.js
@@ -1,10 +1,11 @@
 export default function handleRankat(message, role, client) {
   const filter = m => m.content.startsWith('ja');
-  message.channel.awaitMessages(filter, { min: 1, max: 5, time: 120000 })
+  message.channel.awaitMessages(filter, { min: 1, max: 4, time: 120000 })
     .then(collected => {
       let userList = [...collected].map(x => x[1].author.id)
         .filter((item, pos, self) =>self.indexOf(item) == pos)
         .map(id => message.channel.guild.members.cache.find(member => member.id === id));
+        userList.push(message.member);
       moveUsersToChannel('Testo', userList);
     })
     .catch(collected => console.log(collected.size));
