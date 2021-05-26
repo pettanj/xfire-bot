78043013166c602a046cfabc8325fa5c70749612
Petrus Briland <petrus.jonsson@gmail.com>
2021-05-26 17:45:25 +0200

    Update rankat.js

--- commands/rankat.js
+++ commands/rankat.js
@@ -1,24 +1,18 @@
 export default function handleRankat(message, role, client) {
   let existingChannel = client.channels.cache.find(x => x.name.toLowerCase() === 'testo');
   if (existingChannel) {
-    role.members.forEach(member => {
-      if (member.voice) {
-        try {
-         member.voice.setChannel(existingChannel);
-        } catch (error) {
-        }
-      }
-    })
+    moveToChannel(existingChannel);
   } else {
     message.guild.channels.create('Testo', { type: 'voice' }).then(ch => {
-      role.members.forEach(member => {
-        if (member.voice) {
-          try {
-            member.voice.setChannel(ch);
-          } catch (error) {
-          }
-        }
-      })
+      moveToChannel(ch);
     })
+  }
+
+  function moveToChannel(channel) {
+    role.members.forEach(member => {
+      if (member.voice.channel) {
+        member.voice.setChannel(channel);
+      }
+    });
   }
 }
