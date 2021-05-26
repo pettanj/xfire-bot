35d6b2bed040837321dd33530ab84921c2eb4792
Petrus Briland <petrus.jonsson@gmail.com>
2021-05-26 22:24:43 +0200

    Update rankat

--- commands/rankat.js
+++ commands/rankat.js
@@ -1,18 +1,28 @@
 export default function handleRankat(message, role, client) {
-  let existingChannel = client.channels.cache.find(x => x.name.toLowerCase() === 'testo');
-  if (existingChannel) {
-    moveToChannel(existingChannel);
-  } else {
-    message.guild.channels.create('Testo', { type: 'voice' }).then(ch => {
-      moveToChannel(ch);
+  const filter = m => m.content.startsWith('ja');
+  message.channel.awaitMessages(filter, { min: 1, max: 5, time: 120000 })
+    .then(collected => {
+      let userList = [...collected].map(x => x[1].author.id)
+        .filter((item, pos, self) =>self.indexOf(item) == pos)
+        .map(id => message.channel.guild.members.cache.find(member => member.id === id));
+      moveUsersToChannel('Testo', userList);
     })
-  }
+    .catch(collected => console.log(collected.size));
 
-  function moveToChannel(channel) {
-    role.members.forEach(member => {
-      if (member.voice.channel) {
-        member.voice.setChannel(channel);
+    function moveUsersToChannel (name, users) {
+      let existingChannel = client.channels.cache.find(x => x.name.toLowerCase() === name.toLowerCase());
+      if (existingChannel) {
+        users.forEach(user => moveToChannel(user, existingChannel));
+      } else {
+        message.guild.channels.create(name, { type: 'voice' }).then(ch => {
+          users.forEach(user => moveToChannel(user, ch));
+        })
       }
-    });
+    }
+      
+  function moveToChannel(member, channel) {
+    if (member.voice.channel) {
+      member.voice.setChannel(channel);
+    }
   }
 }
