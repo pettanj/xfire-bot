ba86999431fcc8e0335f3d358e20317968bc1bc2
Petrus Briland <petrus.jonsson@gmail.com>
2021-05-27 17:54:16 +0200

    Update word list and nickname list

--- commands/kojken.js
+++ commands/kojken.js
@@ -1,7 +1,7 @@
 import {moveUsersToChannel } from '../utils.js';
 
-const wordList = ['lol', 'okej', 'eyy'];
-const nickNames = ['Elias Revis', 'Korken', 'K0jk3n']
+const wordList = ['e', 'ee', 'eee', 'eo', 'eoo', 'eooo', 'eeo', 'eeeo', 'eeoo', 'eeooo', 'eeeooo', 'eeeoooo', 'eeeeooo', 'eeeeoooo', 'kmr', 'ej', 'wtf'];
+const nickNames = ['Elias Revis', 'Korken', 'K0jk3n', 'Lillspyan', 'Dikken', 'Elias Flens', 'Gräddbollen', 'Gräddbullen'];
 
 export default async function handleKojken(message, client) {
   if (wordList.includes(message.content.toLowerCase())) {
